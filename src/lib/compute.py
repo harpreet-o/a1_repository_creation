c8abbac262fa943e5d267ebb509a39bb91c399e4
harpreet-o <harpreetct98@gmail.com>
2019-12-05 14:38:14 -0700

    Adding exponent support

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -23,4 +23,6 @@ class Compute:
             product *= item
         print(product)
 
-    
+    def exponent(self):
+        num_exponent = self.operands[0] ** self.operands[1]
+        print(num_exponent)
