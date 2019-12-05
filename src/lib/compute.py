13e7d7561410f3b287400b74d18a53308f69afe2
harpreet-o <harpreetct98@gmail.com>
2019-12-05 14:37:45 -0700

    deleted unnecessary code

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -23,6 +23,4 @@ class Compute:
             product *= item
         print(product)
 
-    def exponent(self):
-        num_exponent = self.operands[0] ** self.operands[1]
-        print(num_exponent)
+    
