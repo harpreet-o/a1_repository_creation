58a1939eb6add3e427233d7fbd8719dafcdefbd9
harpreet-o <harpreetct98@gmail.com>
2019-12-05 23:52:44 -0700

    Add support for distance

--- src/lib/util/util.py
+++ src/lib/util/util.py
@@ -36,12 +36,16 @@ class Util(object):
         pass
 
     @classmethod
-    def speed(arg1, arg2):
-        pass
+    def speed(cls, unit, *args):
+        speed = 0
+        speed = reduce(lambda x, y: x*y, args)
+        return "%s %s" %(speed, unit)
 
     @classmethod
-    def distance(arg1, arg2):
-        pass
+    def distance(unit, *args):
+        distance = 0
+        distance = reduce(lambda x, y: x*y, args)
+        return "%s %s" %(distance, unit)
 
     @classmethod
     def time(arg1, arg2):
